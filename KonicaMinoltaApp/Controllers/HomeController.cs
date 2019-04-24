14db43e2327bcec91c2ad4ade1df9563bcc73c1d
Andrew Ferguson <andrewferguson343@gmail.com>
2019-04-24 00:24:16 -0400

    Added project

---
+++ KonicaMinoltaApp/Controllers/HomeController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Mail;
+using System.Web.Mvc;
+using System.Collections.Generic;
+
+namespace KonicaMinoltaApp.Controllers
+{
+  public class HomeController : Controller
+  {
+    public ActionResult Index()
+    {
+      if(Session["fileData"] == null)
+      {
+        Session["fileData"] = new List<string>();
+      }
+      return View();
+    }
+
+    public ActionResult SendEmail(FormCollection form)
+    {
+      //First, Parse data from the form
+      string detectedBase64String = string.Empty;
+      string originalBase64 = string.Empty;
+      string detectedEmail = string.Empty;
+      string filePath = string.Empty;
+      if (form != null)
+      {
+        if (form.AllKeys.Contains("DataURL"))
+        {
+          detectedBase64String = form["DataURL"];
+        }
+        if (form.AllKeys.Contains("Email"))
+        {
+          detectedEmail = form["Email"];
+        }
+      }
+      // If there is an image, format its base64 string
+      if (detectedBase64String != string.Empty)
+      {
+        originalBase64 = detectedBase64String;
+        detectedBase64String = detectedBase64String.Replace("data:image/png;base64,", "");
+      }
+      filePath = AppDomain.CurrentDomain.BaseDirectory + @"\\Content\\Images\\image" + DateTime.Now.ToLongTimeString().Replace(':', '-') + ".png";
+
+      //create image attachement from base64 string
+      byte[] bytes = Convert.FromBase64String(detectedBase64String);     
+      using (FileStream fs = new FileStream(filePath, FileMode.Create))
+      {
+        using (BinaryWriter bw = new BinaryWriter(fs))
+        {
+          byte[] data = Convert.FromBase64String(detectedBase64String);
+          bw.Write(data);
+          bw.Close();
+        }
+      }
+      
+      Attachment attach = new Attachment(filePath);
+      //create and populate mail message object
+      MailMessage mail = new MailMessage("KMAndrewF@gmail.com", detectedEmail);
+      SmtpClient client = new SmtpClient();
+      client.Port = 25;
+      client.DeliveryMethod = SmtpDeliveryMethod.Network;
+      client.UseDefaultCredentials = false;
+      client.Credentials = new System.Net.NetworkCredential("KMAndrewF@gmail.com", "KMAF9*91");
+      client.EnableSsl = true;
+      client.Host = "smtp.gmail.com";
+      mail.Subject = "Your friend sent you a post card!";
+      mail.Body = "The card is attached!";
+      mail.Attachments.Add(attach);
+      client.Send(mail);
+
+      //For history, Add to session variable
+      List<string> fileData = (List<string>)Session["fileData"];
+      fileData.Add(originalBase64);
+      Session["fileData"] = fileData;
+
+      return View("Index");
+    }
+  }
+}
---
+++ KonicaMinoltaApp/Scripts/app.js
@@ -0,0 +1,76 @@
+﻿//when document is ready, add an event listener to the file
+//input which is hit when the user uplaods file, callback populates the
+//canvas with the users image
+var img = new Image();
+
+
+
+$('#fileUploader').change(function (e) {
+  img.onload = PopulateCanvas;
+  img.src = URL.createObjectURL(this.files[0]);
+  var msg = msg = $('#canvasText').val();
+  GetDataUrl();
+});
+
+function PopulateCanvas() {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var fontSize = $('#fontSize').val();
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  context.canvas.width = img.width;
+  context.canvas.height = img.height;
+  context.drawImage(img, 0, 0);
+
+  msg = $('#canvasText').val();
+  context.textAlign = 'center';
+  context.fillStyle = $('#fontColor').val();
+  context.font = fontSize + " Arial";
+  context.fillText(msg, context.canvas.width / 2, context.canvas.height - 40);
+  GetDataUrl();
+}
+
+function GetDataUrl() {
+  var canvas = document.getElementById('canvas');  
+  var dataUrl = canvas.toDataURL("image/png");
+  var image = document.createElement('img');
+  image.src = dataUrl;
+  $('#canvasImageURL').val(dataUrl);
+ 
+}
+
+function AddText() {
+  var canvas = document.getElementById('canvas');
+  var context = canvas.getContext('2d');
+  var msg = "";
+  context.clearRect(20, 100, 20, 100);
+}
+
+function RestoreHistoryImage(data) {
+  alert(data);
+  img.src = data;
+  PopulateCanvas();
+}
+
+function ValidateSendEmail() {
+  var errorFlag = false;
+  var email = $('#sendEmail-Reciever').val();
+  var data = $('#canvasImageURL').val();
+
+  alert(email);
+  if (email === "" | !email.includes("@")) {
+    errorFlag = true;
+    alert("Email is not entered correctly");
+  }
+  alert(data);
+  if (data === "") {
+    errorFlag = true;
+    alert("No image is loaded");
+  }
+  alert(errorFlag);
+  return !errorFlag;
+}
+
+function HandleSendEmail(e) {
+  location.reload();
+}
